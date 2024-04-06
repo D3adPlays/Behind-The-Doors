56e0654bc9d30bed898a64ff5ce433a8c377583d
Matthieu Witrowiez <pitchounouw@gmail.com>
2024-04-06 20:39:29 +0200

    [ADD] Main menu and room base

---
+++ scenes/main_menu/main_menu.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+@onready var fade_transiton = $FadeTransiton
+@onready var follower = $Background/Follower
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	fade_transiton.play("Fade_In");
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	follower.global_position = get_global_mouse_position();
+	pass
+
+
+func _on_quitter_pressed() -> void:
+	get_tree().quit(0);
+	pass # Replace with function body.
+
+
+func _on_options_pressed() -> void:
+	print("Opening options...");
+	pass # Replace with function body.
+
