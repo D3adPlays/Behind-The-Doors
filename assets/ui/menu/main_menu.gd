697b3ea2e6ee292c76fd22074c850ce09c8736e9
Matthieu Witrowiez <pitchounouw@gmail.com>
2024-04-07 14:30:13 +0200

    [FIX] project structure

--- assets/ui/menu/main_menu.gd
+++ assets/ui/menu/main_menu.gd
@@ -1,10 +1,8 @@
 class_name MainMenu
 
 func _on_options_pressed():
-	get_tree().quit();
 	pass # Replace with function body.
 
 
 func _on_follower_draw():
-	global_position = get_global_mouse_position();
 	pass # Replace with function body.
